c2b553a4a1150c8a4b1fb26dc3f9b6be72a462f5
Alec Elton <alec.elton@gatewayblend.com>
2018-10-12 20:47:22 -0500

    Hello world

---
+++ manage.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python
+
+import logging
+import sys
+import os
+
+from flask_script import Shell
+from flask_migrate import Migrate, MigrateCommand
+
+
+# Create the app/init plugins before we can import commands
+from app.main import get_app
+app = get_app()
+
+from app.main import (
+    manager,
+    db,
+    )
+logger = logging.getLogger('app')
+
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == '__main__':
+    try:
+        manager.run()
+    except Exception as e:
+        logger.critical("Uncaught exception: %s: %s", e.__class__.__name__, str(e), exc_info=True)
+        raise
