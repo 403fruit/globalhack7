b35400d68b26171013d35d1ccb846f19c21e9353
Alec Elton <alec.elton@gatewayblend.com>
2018-10-13 09:47:08 -0500

    User resource model updates

--- app/models/common.py
+++ app/models/common.py
@@ -1,5 +1,8 @@
-from app.main import db
 import sqlalchemy_utils as sau
+
+import arrow
+
+from app.main import db
 
 
 USER_RESOURCE_TYPES = [
@@ -8,7 +11,12 @@ USER_RESOURCE_TYPES = [
 ]
 
 
-class User(db.Model):
+class TimestampMixin(object):
+    created_at = db.Column(sau.ArrowType(), nullable=False, index=True, default=arrow.utcnow)
+    updated_at = db.Column(sau.ArrowType(), nullable=False, index=True, default=arrow.utcnow, onupdate=arrow.utcnow)
+
+
+class User(TimestampMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.BigInteger, primary_key=True)
     username = db.Column(db.String(255), unique=True, nullable=False)
@@ -23,9 +31,22 @@ class Resource(db.Model):
     name = db.Column(db.String(500), index=True)
 
 
-class UserResource(db.Model):
+class UserResource(TimestampMixin, db.Model):
     __tablename__ = 'user_resources'
     id = db.Column(db.BigInteger, primary_key=True)
+    resource_id = db.Column(db.BigInteger, db.ForeignKey('resources.id', onupdate='CASCADE', ondelete='RESTRICT'), nullable=False)
+    resource = db.relationship('Resource', backref='user_resources')
+    user_id = db.Column(db.BigInteger, db.ForeignKey('user.id', onupdate='CASCADE', ondelete='CASCADE'), nullable=False)
+    user = db.relationship('User', backref='resources')
     type = db.Column(sau.ChoiceType(USER_RESOURCE_TYPES), index=True)
-    quantity = db.Column(db.BigInteger)
-    fulfilled = db.Column(db.Boolean, default=False, index=True)
+    quantity_available = db.Column(db.BigInteger)
+    quantity_needed = db.Column(db.BigInteger)
+    quantity_fulfilled = db.Column(db.BigInteger)
+    fulfills_id = db.Column(db.BigInteger, db.ForeignKey('user_resources.id', onupdate='CASCADE', ondelete='CASCADE'))
+    fulfills = db.relationship('UserResource', foreign_keys=[fulfills_id], remote_side=[id], backref='fulfilled_by')
+
+    @property
+    def is_fulfilled(self):
+        if self.quantity_needed is None:
+            return None
+        return self.quantity_needed - self.quantity_fulfilled <= 0
