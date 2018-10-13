fabda425ff514a5ce72a070bae7df43c368f2615
Dylan Williams <dylan.williams@gatewayblend.com>
2018-10-13 15:30:47 -0500

    Don't redirect to en, instead just default lang_code on link if not set

--- app/views/index.py
+++ app/views/index.py
@@ -6,8 +6,6 @@ app = Blueprint('index', __name__)
 
 @app.route('/')
 def index():
-    if g.lang_code is None:
-        return redirect(url_for('index.index', lang_code='en'))
     return render_template('index.jinja.html')
 
 
